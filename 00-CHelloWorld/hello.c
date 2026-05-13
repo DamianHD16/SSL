45149e1ca5196b436ea07f6a0e6a877a0fb90f38
Damián H. Dobarro <ddobarro@frba.utn.edu.ar>
2026-05-13 20:59:44 -0300

    Subo hello.c y output.txt

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+
+    #ifdef __STDC_VERSION__
+        printf("Language version: %ld\n", __STDC_VERSION__);
+    #else
+        printf("__STDC_VERSION__ not defined.");
+    #endif
+
+    printf("Compiler version: %s\n", __VERSION__);
+    return 0;
+}
