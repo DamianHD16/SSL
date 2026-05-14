9c54d5492da42248a6271a1a02646931b27caf9b
Damián H. Dobarro <ddobarro@frba.utn.edu.ar>
2026-05-13 21:04:06 -0300

    Fix formatting of printf statement for STDC_VERSION

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -6,7 +6,7 @@ int main() {
     #ifdef __STDC_VERSION__
         printf("Language version: %ld\n", __STDC_VERSION__);
     #else
-        printf("__STDC_VERSION__ not defined.");
+        printf("__STDC_VERSION__ not defined.\n");
     #endif
 
     printf("Compiler version: %s\n", __VERSION__);
